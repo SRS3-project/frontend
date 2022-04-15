40fb4141861b8e468f881d060c8c2d91bf1e96d9
passwd <passwd@mes3hacklab.org>
2022-04-15 20:27:39 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
