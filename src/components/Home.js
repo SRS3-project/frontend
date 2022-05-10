8c360d72d952791222d0516fd7f75c6e2aeb2255
MCdeamon7 <60098138+MCdeamon7@users.noreply.github.com>
2022-05-10 17:57:07 +0200

    UserBoxMade

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,59 +2,60 @@ import LateralMenu from "./LateralMenu/LateralMenu";
 import TechPanel from "./ElementPanels/TechPanel";
 import ResourcePanel from "./ElementPanels/ResourcePanel";
 import { Columns, Notification } from "react-bulma-components";
-import 'bulma/css/bulma.min.css';
-import Logo from "../images/logo.png"
-
+/* import { Footer } from "react-bulma-components"; */
+import Logo from "../images/logo.png";
+import UserBox from "./UserBox/UserBox";
+import Footer from "./Footer/Footer";
+import { UserProvider } from "../context/UserProvider";
 
 
 function Home() {  
     return (
         <>
-{/*
-        <div class="topInfo">
-            <Header/>
-        </div>
-*/}
-        <br/>
-        <Columns multiline centered>
-            <Columns.Column size={3}>
-                <img src={Logo}/>
-            </Columns.Column>
-            <Columns.Column size={6}>
-                <Notification color="link">
-                    <ResourcePanel/>
-                </Notification>
-                <Columns.Column>
+            <UserProvider>
+                <Columns multiline centered>
+                    <Columns.Column size={3}>
+                        <img src={Logo}/>
+                    </Columns.Column>
+                    <Columns.Column size={6}>
+                        <Notification color="link">
+                            <h1 class="gameName">dr.Ogame</h1>
+                        </Notification>
+                        <Notification color="link">
+                            <ResourcePanel/>
+                        </Notification>
+                    </Columns.Column>
+                    <Columns.Column size={3}>
+                        <Notification color="link">
+                            <UserBox/>
+                        </Notification>
+                    </Columns.Column>
+
+                    <Columns.Column size={4}>
+                        <Notification color="link">
+                            <LateralMenu />
+                        </Notification>
+                    </Columns.Column>
+                    <Columns.Column size="auto">
+                        <Notification color="link">
+                            <h1>Home</h1>
+                            <p>You are logged in!</p>
+                        </Notification>
+                        <br />
+                        <Notification color="link">
+                            <TechPanel/>
+                        </Notification>
+                    </Columns.Column >
+                </Columns>
+
+                <Columns.Column size={12}>
                     <Notification color="link">
-                        <h1 class="gameName">dr.Ogame</h1>
+                        <Footer/>
                     </Notification>
                 </Columns.Column>
-            </Columns.Column>
-            <Columns.Column size={3}>
-                <h1> Aggiungere Pannello Utente</h1>
-            </Columns.Column>
-
-            <Columns.Column size={4}>
-                <Notification color="link">
-                    <LateralMenu />
-                </Notification>
-            </Columns.Column>
-            <Columns.Column size="auto">
-                <Notification color="link">
-                    <h1>Home</h1>
-                    <p>You are logged in!</p>
-                </Notification>
-                <br />
-                <Notification color="link">
-                    <TechPanel/>
-                </Notification>
-            </Columns.Column >
-        </Columns>
-{/*
-        <div class="booty">
-            <Footer/>
-        </div>
-*/}
+            
+            </UserProvider>
+   
         </>
     );
 }
