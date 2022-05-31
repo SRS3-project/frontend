e67cf0da29c3b202ccf217940e199f1e9a341f1f
MCdeamon7 <60098138+MCdeamon7@users.noreply.github.com>
2022-05-31 22:37:45 +0200

    added counter under resourcePanel

--- src/components/Panels/ResourcePanel.js
+++ src/components/Panels/ResourcePanel.js
@@ -12,44 +12,48 @@ import food from "../../images/food-icon.png";
 import useUser from "../../hooks/useUser";
 
 const resources = [
-	{ image: food, name: "FOOD" },
-	{ image: gold, name: "GOLD" },
-	{ image: minerals, name: "MINERALS" },
-	{ image: wood, name: "WOOD" },
+	{ image: food, name: "FOOD", rate: 10 },
+	{ image: gold, name: "GOLD", rate: 15 },
+	{ image: minerals, name: "MINERALS", rate: 30 },
+	{ image: wood, name: "WOOD", rate: 20 },
 ];
 
 const ResourcePanel = () => {
 	const { user } = useUser();
 
 	const isUserValid = () => {
-		return (
-			!(Object.is(user, null) || Object.is(user, undefined) || Object.keys(user).length === 0)
-		)
-	}
-	
+		return !(
+			Object.is(user, null) ||
+			Object.is(user, undefined) ||
+			Object.keys(user).length === 0
+		);
+	};
+
 	const setLabel = (resource) => {
 		return resource.name.charAt(0) + resource.name.slice(1).toLowerCase();
-	}
-	
+	};
+
 	return (
 		<>
 			<ContainerBox>
 				{resources.map((resource, i) => (
-						
-						<li key={resource.name}>
-							<ItemBox
-								topLabel={setLabel(resource)}
-								image={resource.image}
-								bottomLabel={
-									//Object.keys(user).length === 0
-									isUserValid()
-										? user.resources[i].amount
-										: "0"
-								}
-							/>
-						</li>
-					)
-				)}
+					<li key={resource.name}>
+						<ItemBox
+							topLabel={setLabel(resource)}
+							image={resource.image}
+							bottomLabel={
+								//Object.keys(user).length === 0
+								isUserValid() ? user.resources[i].amount : "0"
+							}
+						/>
+						<p>
+							{resource.rate *
+								(isUserValid() ? user.level : 0) *
+								Math.pow(1.1, user.level).toFixed(2)}
+							/h
+						</p>
+					</li>
+				))}
 			</ContainerBox>
 		</>
 	);
