08f287f081914d703d7122d03dc3c7a4cc04e8f6
Luigi Francesco Ricatti <ricattilf@gmail.com>
2022-06-05 13:48:53 +0200

    try to fix resources update

--- src/components/Panels/ResourcePanel.js
+++ src/components/Panels/ResourcePanel.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bulma/sass/utilities/_all.sass";
 import "bulma/sass/components/menu.sass";
 import ItemBox from "../ItemBox/ItemBox";
 import ContainerBox from "../ContainerBox/ContainerBox";
+import { useState } from "react";
 
 import minerals from "../../images/resources/mineral-icon.png";
 import wood from "../../images/resources/wood-icon.png";
@@ -22,9 +23,41 @@ const resources = [
 const ResourcePanel = () => {
 	const { user } = useUser();
 
+	const [food,setFood] = useState(0);
+	const [gold,setGold] = useState(0);
+	const [minerals,setMinerals] = useState(0);
+	const [wood,setWood] = useState(0);
+
 	const setLabel = (resource) => {
 		return resource.name.charAt(0) + resource.name.slice(1).toLowerCase();
 	};
+
+	const getResourceValue = (index) => {
+		switch(index) {
+			case 0: return food;
+			case 1: return gold;
+			case 2: return minerals;
+			case 3: return wood;
+		}
+	}
+
+	const getResourceFromUser = () => {
+		if(IsValid(user)) {
+			setFood(user.resources[0].amount);
+			setGold(user.resources[1].amount);
+			setMinerals(user.resources[2].amount);
+			setWood(user.resources[3].amount);
+		} else {
+			setFood(food);
+			setGold(gold);
+			setMinerals(minerals);
+			setWood(wood);
+		};		
+	}
+
+	useEffect(() => {
+		getResourceFromUser()
+	},[user]);
 
 	return (
 		<>
@@ -35,7 +68,7 @@ const ResourcePanel = () => {
 							topLabel={setLabel(resource)}
 							image={resource.image}
 							bottomLabel={
-								IsValid(user) ? user.resources[i].amount : "0"
+								getResourceValue(i)
 							}
 						/>
 						<p>
